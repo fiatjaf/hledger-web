9f9083f4d61b5dd1b24482260f5d1b0dc3df4e00
fiatjaf <fiatjaf@gmail.com>
2018-01-25 22:13:19 -0200

    real-time journal parsing from a textarea.

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,7 @@
 module Main where
 
 import Debug.Trace
+import Prelude hiding (unlines)
 import Data.Text hiding (pack, map)
 import Hledger.Read (readJournal)
 import Hledger.Data.Journal
@@ -11,11 +12,12 @@ import Hledger.Data.Transaction
 import Hledger.Data.Types (Journal)
 import GHCJS.Types
 import Miso
-import Miso.String (pack, toMisoString, MisoString)
+import Miso.String (pack, toMisoString, fromMisoString, MisoString)
 
 data Model
   = Model
-    { journal :: Maybe Journal
+    { unparsed :: MisoString
+    , journal :: Maybe Journal
     , err :: Maybe MisoString
     } deriving (Eq, Show)
 
@@ -23,7 +25,7 @@ main :: IO ()
 main = startApp App {..}
   where
     initialAction = ParseJournal
-    model  = Model Nothing Nothing
+    model  = Model initialJournal Nothing Nothing
     update = updateModel
     view   = viewModel
     events = defaultEvents
@@ -32,20 +34,18 @@ main = startApp App {..}
 
 data Action
   = NoOp
-  | SayHelloWorld
+  | SetUnparsedJournal MisoString
   | ParseJournal
   | ParsedJournal (Either String Journal)
   deriving (Show, Eq)
 
 updateModel :: Action -> Model -> Effect Action Model
 updateModel NoOp m = noEff m
-updateModel SayHelloWorld m = m <# do
-  putStrLn "Hello World!" >> pure NoOp
-updateModel ParseJournal m = m <# do
-  ParsedJournal <$> parseJournal "\
-\2018/1/1 nada \n\
-\  jambo  12 \n\
-\  caca \n"
+updateModel (SetUnparsedJournal uj) m =
+  updateModel ParseJournal m { unparsed = uj }
+updateModel ParseJournal (Model {..}) = Model {..} <# do
+  putStr (fromMisoString unparsed)
+  ParsedJournal <$> parseJournal unparsed
 updateModel (ParsedJournal res) m = noEff updatedModel
   where
     updatedModel = case res of
@@ -54,30 +54,34 @@ updateModel (ParsedJournal res) m = noEff updatedModel
 
 viewModel :: Model -> View Action
 viewModel Model {..} = div_ []
-  [ button_ [ onClick SayHelloWorld ] [ text $ pack "hello" ]
-  , div_ []
+  [ div_ []
     [ case err of
       Nothing -> text $ pack "no errors" 
       Just err -> text $ toMisoString err
     ]
   , div_ []
-    $ case journal of
-      Nothing -> []
-      Just jrnl ->
-        [ div_ []
-          [ h1_ [] [ text $ pack "account names" ]
-          , ul_ []
-            $ map (li_ [] . (:[]) . text . toMisoString) (journalAccountNames jrnl)
-          ]
-        , div_ []
-          [ h1_ [] [ text $ pack "first transaction" ]
-          , ul_ []
-            [ case journalTransactionAt jrnl 1 of
-              Nothing -> text $ pack ""
-              Just txn -> pre_ [] [ text $ toMisoString (showTransaction txn) ]
+    [ div_ []
+      [ textarea_ [ onInput SetUnparsedJournal ] [ text unparsed ]
+      ]
+    , div_ []
+      $ case journal of
+        Nothing -> []
+        Just jrnl ->
+          [ div_ []
+            [ h1_ [] [ text $ pack "account names" ]
+            , ul_ []
+              $ map (li_ [] . (:[]) . text . toMisoString) (journalAccountNames jrnl)
+            ]
+          , div_ []
+            [ h1_ [] [ text $ pack "first transaction" ]
+            , ul_ []
+              [ case journalTransactionAt jrnl 1 of
+                Nothing -> text $ pack ""
+                Just txn -> pre_ [] [ text $ toMisoString (showTransaction txn) ]
+              ]
             ]
           ]
-        ]
+    ]
   ]
 
 -- foreign import javascript unsafe "$r = new RemoteStorage({logging: true})"
@@ -89,5 +93,17 @@ viewModel Model {..} = div_ []
 -- foreign import javascript unsafe "$1.caching.enable($2)"
 --   rsCache :: JSVal -> Text -> IO ()
 
-parseJournal :: Text -> IO (Either String Journal)
-parseJournal jrnl = readJournal (Just "journal") Nothing True Nothing jrnl
+parseJournal :: MisoString -> IO (Either String Journal)
+parseJournal jrnl =
+  readJournal (Just "journal") Nothing True Nothing (fromMisoString jrnl)
+
+initialJournal :: MisoString
+initialJournal = toMisoString $ unlines
+  [ "2018/2/7 got paid"
+  , "  assets:money  1200"
+  , "  income"
+  , ""
+  , "2018/1/18 coffee"
+  , "  assets:money  -12"
+  , "  expenses:food"
+  ]
